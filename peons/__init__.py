9423d68ef2f16e710826e94a375118308e1282a7
Harald Klimach <harald.klimach@dlr.de>
2025-04-03 09:47:05 +0200

    Import peon scripts from treelm

---
+++ peons/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from peons.harvest_series import run as harvest_run
+from peons.createDirPerTimeStampForPVTU import run as createTimeStampDir
+from peons.createPVDfromPVTU import run as createPVD
